7b4c4b87203cc1c3fad5a4f6b8e00521da20d5d2
Divya <srivastava.divya7@gmail.com>
2017-09-06 12:34:56 +0530

    Generate Migrations of evaluate_ticket

---
+++ main/database/migrations/2017_09_06_065133_create_evaluate_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluateTicketsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('evaluate_tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('settings_id')->comment = "This refers to point value settings";
+            $table->integer('users_id');
+            $table->integer('revision_id')->comment = "user id of those who are Project Manager and Responsible";
+            $table->integer('project_id');
+            $table->integer('months');
+            $table->integer('year');
+            $table->enum('status', ['enable', 'disable']);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('evaluate_tickets');
+    }
+}
